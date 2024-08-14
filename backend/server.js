e8237276a4e6a733dd95b05f3f9b7a9c04bc570e
Cahangir_Asgerov <esgerovcahangir12@gmail.com>
2024-08-15 00:25:33 +0400

    Server.js will be changed

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT;
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["https://bostorek-az-frontend.onrender.com"],
   credentials: true,
 };
 
